5fc42e932cbac6c87c7d3759fb7f5cf61182abfd
Daan Bergman <daan.bergman@kpnmail.nl>
2023-12-07 00:20:45 +0100

    audio tracking

--- OurMisty/main.py
+++ OurMisty/main.py
@@ -5,7 +5,6 @@ from ttsazure import CustomSpeak
 from deepface import DeepFace
 import numpy as np
 import cv2
-
 import time
 import base64
 
@@ -13,18 +12,20 @@ import base64
 azure_key = "5cea5747535f427b889651b335da74f7"
 azure_region = "westeurope"
 misty = Robot("192.168.2.92")
-misty.set_default_volume(30)
+misty.set_default_volume(15)
 
 
 speak = CustomSpeak(misty, azure_key, azure_region) #for custom voice
 #speak = misty #for built in voice
 
-# misty's head position
+# misty's position
 headPitch = 0
 headYaw = 0
+bodyYaw = 0
 
 # perceived emotional state
 emotion = "neutral"
+displayed_emotion = "neutral"
 
 # Connect to Character.ai
 charAI = CharAI()
@@ -40,13 +41,13 @@ def test_head_positions():
 
 def tts_complete(event):
     print("tts", event)
-    misty.capture_speech_azure(silenceTimeout=10000, maxSpeechLength=20000, speechRecognitionLanguage="nl-NL", azureSpeechKey=azure_key, azureSpeechRegion=azure_region)
+    misty.capture_speech_azure(speechRecognitionLanguage="nl-NL", azureSpeechKey=azure_key, azureSpeechRegion=azure_region)
 
 def voice_record_complete( event):
     print("vrc", event)
     if "message" in event:
         message = event["message"]
-        if message["errorMessage"] == "NoMatch":
+        if not message["success"]:
             speak.speak("Sorry, kan je dat herhalen?", utteranceId="utterance")
             return
         result = message["speechRecognitionResult"]
@@ -54,6 +55,19 @@ def voice_record_complete( event):
         response = charAI.get_response(result)
         print(f"misty's response: {response}")
         speak.speak(response, utteranceId="utterance")
+
+
+def look_at_audio(event):
+    print(event)
+    global bodyYaw
+    if "message" in event:
+        message = event["message"]
+        heading = message["degreeOfArrivalSpeech"]
+        heading2 = np.argmax(message["voiceActivityPolar"])
+        print(f"bodyYaw: {bodyYaw}, heading: {heading}, heading2: {heading2}")
+        print(f"changing bodyYaw to: {(bodyYaw - heading) % 360}")
+        result = misty.drive_arc(heading=(bodyYaw - heading) % 360, radius=0, timeMs=3000, reverse=False)
+        print(result.json())
 
 
 # work in progress
@@ -72,7 +86,7 @@ def look_at_face(event):
         if abs(bearing) > 3:
             newYaw = headYaw + bearing * 2
         misty.move_head(newPitch, 0, newYaw)
-        time.sleep(0.5)
+        #time.sleep(0.5)
         detect_emotion_basic()
 
 def detect_emotion_basic():
@@ -83,10 +97,32 @@ def detect_emotion_basic():
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     analysis = DeepFace.analyze(img, actions=("emotion"))
     emotion = analysis[0]["dominant_emotion"]
+    #display_emotion(emotion) #mimic detected emotion
     print(emotion)
 
-def update_position(event):
-    #print(event)
+def display_emotion(emotion):
+    global displayed_emotion
+    if displayed_emotion == emotion:
+        return
+    match emotion:
+        case "neutral":
+            misty.display_image("e_DefaultContent.jpg")
+        case "sad":
+            misty.display_image("e_Sadness.jpg")
+        case "angry":
+            misty.display_image("e_Anger.jpg")
+        case "surprised":
+            misty.display_image("e_Surprise.jpg")
+        case "fear":
+            misty.display_image("e_Fear.jpg")
+        case "happy":
+            misty.display_image("e_Joy.jpg")
+        case "disgust":
+            misty.display_image("e_Disgust.jpg")
+    displayed_emotion = emotion
+
+
+def update_position_head(event):
     global headYaw
     global headPitch
     if "message" in event:
@@ -96,6 +132,13 @@ def update_position(event):
         if message["sensorId"] == "ahp":
             headPitch = message["value"]
 
+def update_position_body(event):
+    global bodyYaw
+    if "message" in event:
+        message = event["message"]
+        bodyYaw = message["yaw"]
+
+
 
 
 
@@ -104,12 +147,14 @@ def start_session():
     misty.stop_object_detector()
     misty.stop_face_detection()
     misty.unregister_all_events()
-    misty.move_head(0,0,0)
+    misty.start_action("body-reset", useVisionData=False)
     misty.register_event(Events.VoiceRecord, "voicerecord", keep_alive=True, callback_function=voice_record_complete)
     misty.register_event(Events.AudioPlayComplete, "ttscomplete", keep_alive=True, callback_function=tts_complete)
     misty.register_event(Events.TextToSpeechComplete, "ttscomplete2", keep_alive=True, callback_function=tts_complete)
-    misty.register_event(Events.ActuatorPosition, "actuatorposition", keep_alive=True, callback_function=update_position)
-    misty.register_event(Events.FaceRecognition, "facedetection", keep_alive=True, debounce=4000, callback_function=look_at_face)
+    misty.register_event(Events.ActuatorPosition, "actuatorposition", keep_alive=True, callback_function=update_position_head)
+    misty.register_event(Events.FaceRecognition, "facedetection", keep_alive=True, debounce=1000, callback_function=look_at_face)
+    misty.register_event(Events.SourceTrackDataMessage, "audiolocalization", keep_alive=True, debounce=2000, callback_function=look_at_audio)
+    misty.register_event(Events.IMU, "imu", keep_alive=True, debounce=1000, callback_function=update_position_body)
     print("starting session")
     misty.start_face_detection()
     speak.speak("Hallo, ik ben Misty! Hoe gaat het met jou?", utteranceId="utterance")
